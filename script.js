5b8cb92fa16b9d1e6f6ea663048a75e941305060
Jason Shi <jshigoodies@gmail.com>
2023-01-11 17:39:36 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// variables
+
+
+
+function getWeatherApi() {
+    var apiURL = "api.openweathermap.org/data/2.5/forecast/daily?lat=33.7488&lon=84.3877&cnt=7&appid=b4781e0ceea501131352020f78d958b4"
+}
